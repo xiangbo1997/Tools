e53d617253cfbfee3c4d2a8c35620610d32d07e6
沙漠与旅人 <3301379698@qq.com>
2021-04-18 09:34:57 +0800

    模拟的循环定时器

--- tools.js
+++ tools.js
@@ -1,3 +1,23 @@
-let a = 0;
-let b = 1;
-let c = 3;
+
+// 常用的工具函数
+
+/**
+ * settimeout 模拟 setInterval
+ */
+class ImitateInterval {
+    timerId
+    startWork(callBack, time) {
+      callBack();
+      const fn = () => {
+        clearTimeout(this.timerId);
+        this.timerId = setTimeout(() => {
+          callBack();
+          fn();
+        }, time)
+      };
+      fn();
+    }
+    endWork() {
+      clearTimeout(this.timerId);
+    }
+  }
