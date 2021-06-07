4426555b9fc455f54d1e1569441fe1555a32858e
沙漠与旅人 <3301379698@qq.com>
2021-06-07 22:20:08 +0800

    一维数组转换二维数组

--- tools.js
+++ tools.js
@@ -20,4 +20,32 @@ class ImitateInterval {
     endWork() {
       clearTimeout(this.timerId);
     }
-  }
+}
+/**
+ * 
+ * @param {*} sourceArray 原数组
+ *  *一维数组转换成二维数组
+ * [1,2,3,4,5,6,7,8]-> [[1,2],[3,4],[5,6],[7,8]];
+ */
+const OneToTwoArry = (sourceArray) =>{
+  const targetArry = [];
+  let transfer = [];
+  sourceArray.forEach((ele,index)=>{
+    transfer[index%2] = Number(ele);
+    if(transfer.length === 2){
+      targetArry.push(transfer);
+      transfer= [];
+    }
+  })
+ return targetArry;
+}
+ const autoPlay = (() => {
+   const dom1 = document.getElementById('ifr').contentWindow.document.querySelector('.qrcodebox');
+   setInterval(() => {
+    if (dom1 && dom1.style.display === 'block') {
+      const dom = document.getElementById('ifr').contentWindow.document.querySelector('.nextlink');
+      dom && dom.click();
+    }
+   }, 10000);
+  
+})()
